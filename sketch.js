78858c38ca64a69fa4c645204cb82a203fb21763
WoofPuppyWoof <116989493+WoofPuppyWoof@users.noreply.github.com>
2023-01-10 16:40:32 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ var totalCities = 8;
 var recordDistance;
 var bestEver;
 
+var colorGreen = 0;
+
 function setup() {
   createCanvas(400, 300);
   for (var i = 0; i < totalCities; i++) {
@@ -14,12 +16,15 @@ function setup() {
   var d = calcDistance(cities);
   recordDistance = d;
   bestEver = cities.slice();
-
 }
 
 function draw() {
   background(0);
-  fill(255);
+  if (colorGreen == 1) { // hacker mode
+    fill(50, 205, 50);
+  } else {
+    fill(255);
+  }
   for (var i = 0; i < cities.length; i++) {
     ellipse(cities[i].x, cities[i].y, 8, 8);
   }
@@ -33,7 +38,11 @@ function draw() {
   }
   endShape();
 
-  stroke(255, 0, 255); // purple
+  if (colorGreen == 1) {
+    stroke(0, 255, 0)
+  } else {
+    stroke(255, 0, 255); // purple
+  }
   strokeWeight(4);
   noFill();
   beginShape();
@@ -72,7 +81,15 @@ function calcDistance(points) {
   return sum;
 }
 
-
+function keyPressed() {
+  if (keyCode == 72) {
+    if (colorGreen == 0) {
+      colorGreen = 1;
+    } else {
+      colorGreen = 0;
+    }
+  }
+}
 
 
 
